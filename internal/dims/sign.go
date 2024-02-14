a1746957eb98d86826b759bd9a12911d6dd04212
Jeremy Collins <jeremy.collins@beetlebug.org>
2024-02-13 19:55:18 -0500

    Cleanup

--- internal/dims/sign.go
+++ internal/dims/sign.go
@@ -23,24 +23,18 @@ import (
 	"strings"
 )
 
-/* Sign an image URL
- *
- * The signature is the first 7 characters of an md5 hash of:
- *
- *   1. timestamp
- *   2. secret key (from the configuration)
- *   3. commands (as-is from the URL)
- *   4. image URL
- *
- * Example:
- *
- *   1. timestamp: 1234567890
- *   2. secret: mysecret
- *   3. commands: resize/100x100/crop/100x100
- *   4. image URL: http://example.com/image.jpg
- *
- *   md5(1234567890mysecretresize/100x100crop/100x100http://example.com/image.jpg)
- */
+/*
+Sign an image URL
+
+The signature is the first 7 characters of an md5 hash of:
+
+ 1. timestamp
+ 2. secret key (from the configuration)
+ 3. commands (as-is from the URL)
+ 4. image URL
+
+md5(1234567890mysecretresize/100x100crop/100x100http://example.com/image.jpg)
+*/
 func Sign(timestamp string, secret string, commands string, imageUrl string) string {
 	sanitizedCommands := strings.ReplaceAll(commands, " ", "+")
 

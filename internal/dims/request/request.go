2b2f16b41c66d251fbe12f89301080bc1a0accf8
Jeremy Collins <jeremy.collins@beetlebug.org>
2025-05-14 22:09:31 -0400

    Remove unused interfaces

--- internal/dims/request/request.go
+++ internal/dims/request/request.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Jeremy Collins. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package request
 
 import (
@@ -23,19 +37,6 @@ import (
 	"github.com/beetlebugorg/go-dims/internal/gox/imagex/colorx"
 	"github.com/davidbyttow/govips/v2/vips"
 )
-
-type ErrorImageGenerator interface {
-	GenerateErrorImage(w http.ResponseWriter, status int, message string)
-}
-
-type ImageProcessor interface {
-	ProcessImage() (string, []byte, error)
-}
-
-type Signer interface {
-	ValidateSignature(request Request) bool
-	Sign(request Request) string
-}
 
 type Request struct {
 	HttpRequest            http.Request

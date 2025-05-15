409470064669da147b2021ffcb689f808dc4e7b4
Jeremy Collins <jeremy.collins@beetlebug.org>
2025-05-15 19:41:49 -0400

    Add lambda deploy target

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,5 @@
 variable "aws_region" {
   description = "The AWS region to deploy to."
   type        = string
-}
-
-variable "bucket_name" {
-  description = "The name of the source S3 bucket."
-  type        = string
+  default     = "us-east-1"
 }
